9d0b110ed97a2af35b790bfd3a7f7b29ab87866d
Lucas Cifuentes <lucas.cifuentes@osanasalud.com>
2025-02-07 22:24:08 -0300

    add prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,10 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    // "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -12,6 +13,48 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        welcome: "url('../public/images/choiz_bg_img.jfif')"
+      },
+      backgroundColor: {
+        primary: "#292929"
+      },
+      textColor: {
+        primary_600: "#6042AA",
+        primary_300: "#9F7CF7",
+        secondary_600: "#424B54",
+        secondary_300: "#666768"
+      },
+      padding: {
+        1: "8px",
+        2: "16px",
+        3: "24px",
+        4: "32px",
+        5: "40px",
+        6: "48px"
+      },
+      gap: {
+        1: "8px",
+        2: "16px",
+        3: "24px",
+        4: "32px",
+        5: "40px",
+        6: "48px"
+      },
+      margin: {
+        1: "8px",
+        2: "16px",
+        3: "24px",
+        4: "32px",
+        5: "40px",
+        6: "48px"
+      },
+      fontSize: {
+        xl: "26px",
+        l: "20px",
+        m: "16px",
+        s: "14px"
+      }
     },
   },
   plugins: [],
